a8bb1d4f6527f1d3a9612b2439461f2f773bff40
Mark Hepburn <Mark.Hepburn@gmail.com>
2009-12-01 19:34:08 +1100

    Updating configuration for latest haskell-mode.

--- custom-haskell.el
+++ custom-haskell.el
@@ -5,9 +5,7 @@
 (add-to-list 'load-path (concat *mh/lisp-base* "haskell-mode"))
 (load-library "haskell-site-file")
 
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
-;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-hugs)
-;; (setq haskell-hugs-program-name "/usr/local/bin/hugs")
-(setq haskell-hugs-program-name "/usr/local/bin/ghci")
+;; (setq haskell-hugs-program-name "/usr/local/bin/ghci")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
