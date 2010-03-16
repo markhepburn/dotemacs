f38028f9da122a5353e4480fc2c782a6181d5079
Mark Hepburn <Mark.Hepburn@csiro.au>
2010-03-16 17:05:48 +1100

    Adding virtualenv.el to run python within a venv (slightly hacked from original).  Needs to use python-shell instead of run-python (which is a bit of a pain, and will be fixed)

--- custom-python.el
+++ custom-python.el
@@ -8,6 +8,8 @@
                (lambda () (imenu-add-to-menubar "Declarations")))
      (add-hook 'python-mode-hook
                (lambda () (semantic-decoration-mode -1)))
+     (add-hook 'python-mode-hook
+               (lambda () (require 'virtualenv nil t)))
      ;; restore python's backspace binding after it is clobbered by my autopairs stuff:
      (add-hook 'python-mode-hook
                (lambda () (local-set-key (kbd "<backspace>") 'python-backspace)))
