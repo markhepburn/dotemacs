35f26e1d95adf348edb825b26c511e0a2ab91d02
Mark Hepburn <Mark.Hepburn@csiro.au>
2010-04-12 15:08:33 +1000

    Customising nxml hooks; disabling key binding because a functionally-equivalent one already exists (C-M-n), and adding one to enable hide-show mode (since the existing outline support is pretty limited in terms of the elements it supports, and how it supports them).

--- custom-xml.el
+++ custom-xml.el
@@ -7,9 +7,23 @@
              (cons (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss") t) "\\'")
                    'nxml-mode))
 (fset 'xml-mode 'nxml-mode)
-(add-hook 'nxml-mode-hook
-          ;; alternatively, see variable nxml-sexp-element-flag
-          (lambda () (local-set-key (kbd "C-c M-f") 'nxml-forward-balanced-item)))
+;;; Hook(s):
+(defun mh/nxml-enable-hs ()
+  "Functions to run when in nxml mode."
+  (setq nxml-sexp-element-flag t)
+  (hs-minor-mode 1))
+(eval-after-load "hideshow"
+  (let ((nxml-mode-hs-info '(nxml-mode ("^\\s-*\\(<[^/].*>\\)\\s-*$" 1)
+                                       "^\\s-*</.*>\\s-*$")))
+    (when (not (member nxml-mode-hs-info hs-special-modes-alist))
+      (setq hs-special-modes-alist
+            (cons nxml-mode-hs-info hs-special-modes-alist)))))
+(add-hook 'nxml-mode-hook 'mh/nxml-enable-hs)
+;; (add-hook 'nxml-mode-hook
+;;           ;; alternatively, see variable nxml-sexp-element-flag
+;;           ;; Actually, C-M-n/p does what I wanted this for I think!
+;;           (lambda () (local-set-key (kbd "C-c M-f") 'nxml-forward-balanced-item)))
+;;; General setup:
 (setq nxhtml-global-minor-mode t)
 (setq nxhtml-global-validation-header-mode t)
 (setq nxhtml-skip-welcome t)
