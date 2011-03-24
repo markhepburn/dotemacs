743d79b3960adf0f2713f7d9dee58cf2b43c595e
Mark Hepburn <Mark.Hepburn@gmail.com>
2011-03-24 12:09:10 +1100

    Tidying up the ess-tooltip function.

--- custom-stats.el
+++ custom-stats.el
@@ -14,6 +14,7 @@
 (eval-after-load "ess-mode"
   '(progn 
      ;; http://www.sigmafield.org/2009/10/01/r-object-tooltips-in-ess/
+     ;; Tidied up a fair bit (redunant progns removed, using with-temp-buffer)
      (setq ess-R-object-tooltip-alist
            '((numeric    . "summary")
              (factor     . "table")
@@ -25,29 +26,24 @@
        "Get info for object at point, and display it in a tooltip."
        (interactive)
        (let ((objname (current-word))
-             (curbuf (current-buffer))
-             (tmpbuf (get-buffer-create "**ess-R-object-tooltip**")))
+             (curbuf (current-buffer)))
          (if objname
-             (progn
-               (ess-command (concat "class(" objname ")\n")  tmpbuf )
-               (set-buffer tmpbuf)
+             (with-temp-buffer
+               (ess-command (concat "class(" objname ")\n")  (current-buffer) )
                (let ((bs (buffer-string)))
                  (if (not(string-match "\(object .* not found\)\|unexpected" bs))
                      (let* ((objcls (buffer-substring
                                      (+ 2 (string-match "\".*\"" bs))
                                      (- (point-max) 2)))
-                            (myfun (cdr(assoc-string objcls
-                                                     ess-R-object-tooltip-alist))))
-                       (progn
-                         (if (eq myfun nil)
-                             (setq myfun
-                                   (cdr(assoc-string "other"
-                                                     ess-R-object-tooltip-alist))))
-                         (ess-command (concat myfun "(" objname ")\n") tmpbuf)
-                         (let ((bs (buffer-string)))
-                           (progn
-                             (set-buffer curbuf)
-                             (tooltip-show-at-point bs 0 30)))))))))
-         (kill-buffer tmpbuf)))
+                            (myfun (cdr (assoc-string objcls
+                                                      ess-R-object-tooltip-alist))))
+                       (unless myfun
+                         (setq myfun
+                               (cdr(assoc-string "other"
+                                                 ess-R-object-tooltip-alist))))
+                       (ess-command (concat myfun "(" objname ")\n") (current-buffer))
+                       (let ((bs (buffer-string)))
+                         (set-buffer curbuf)
+                         (tooltip-show-at-point bs 0 30)))))))))
 
      (define-key ess-mode-map (kbd "C-c .") 'ess-R-object-tooltip)))
