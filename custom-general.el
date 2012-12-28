818305b3a360c1bc24ca59e525072a9a847b55e0
Mark Hepburn <Mark.Hepburn@gmail.com>
2012-12-28 22:26:39 +1100

    Using the ac-derived popup library for yas dropdown menu.

--- custom-general.el
+++ custom-general.el
@@ -134,6 +134,21 @@
 ;;; Code templating:
 (when (require 'yasnippet nil t)
   (yas-global-mode 1)
+  ;; http://iany.me/2012/03/use-popup-isearch-for-yasnippet-prompt/
+  (defun yas-popup-isearch-prompt (prompt choices &optional display-fn)
+    (when (featurep 'popup)
+      (popup-menu*
+       (mapcar
+        (lambda (choice)
+          (popup-make-item
+           (or (and display-fn (funcall display-fn choice))
+               choice)
+           :value choice))
+        choices)
+       :prompt prompt
+       ;; start isearch mode immediately
+       :isearch t)))
+  (setq yas-prompt-functions '(yas-popup-isearch-prompt yas-no-prompt))
   ;; 't to jit-load snippets:
   (yas-load-directory (concat *mh/lisp-base* "snippets") t))
 
