a2595f90635d3ea8e9dac858a11f6d57fe33f733
Mark Hepburn <Mark.Hepburn@gmail.com>
2013-01-25 15:42:43 +1100

    Add automatic push and pull for mobile-org interaction.

--- custom-org.el
+++ custom-org.el
@@ -38,6 +38,49 @@
                                   org-agenda-files))
      (setq org-mobile-inbox-for-pull (expand-file-name "from-mobile.org" org-directory))
 
+     ;; Automatic syncing (push + pull) for mobile-org (see
+     ;; https://github.com/matburt/mobileorg-android/wiki/FAQ)
+     (defvar org-mobile-push-timer nil
+       "Timer that `org-mobile-push-timer' used to reschedule itself, or nil.")
+
+     (defun org-mobile-push-with-delay (secs)
+       (when org-mobile-push-timer
+         (cancel-timer org-mobile-push-timer))
+       (setq org-mobile-push-timer
+             (run-with-idle-timer
+              (* 1 secs) nil 'org-mobile-push)))
+
+     (add-hook 'after-save-hook
+               (lambda ()
+                 (when (eq major-mode 'org-mode)
+                   (dolist (file (org-mobile-files-alist))
+                     (if (string= (expand-file-name (car file)) (buffer-file-name))
+                         (org-mobile-push-with-delay 30))))))
+
+     ;; refreshes agenda file each day:
+     (run-at-time "00:05" (* 24 60 60)
+                  '(lambda () (org-mobile-push-with-delay 1)))
+
+     (org-mobile-pull) ;; run org-mobile-pull at startup
+
+     (defun install-monitor (file secs)
+       (run-with-timer
+        0 secs
+        (lambda (f p)
+          (unless (< p (second (time-since (elt (file-attributes f) 5))))
+            (org-mobile-pull)))
+        file secs))
+
+     (install-monitor (file-truename
+                       (concat
+                        (file-name-as-directory org-mobile-directory)
+                        org-mobile-capture-file))
+                      5)
+
+     ;; Do a pull every 5 minutes to circumvent problems with timestamping
+     ;; (ie. dropbox bugs)
+     (run-with-timer 0 (* 5 60) 'org-mobile-pull)
+
      ;; Don't use agenda-cycle at the moment, so rebind C-, to my
      ;; scrolling commands:
      (add-hook 'org-mode-hook
