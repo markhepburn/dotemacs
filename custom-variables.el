50ec14d53e2adc8435dc644c2806a72fa6039a35
Mark Hepburn <Mark.Hepburn@gmail.com>
2013-02-08 11:59:54 +1100

    Checking local changes to custom-variables in; first step in making it system-dependent.

--- custom-variables.el
+++ custom-variables.el
@@ -10,7 +10,7 @@
  '(haskell-notify-p t)
  '(haskell-process-type (quote cabal-dev))
  '(haskell-tags-on-save t)
- '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "/home/hep016/Projects/connie/connie2testing/venv/bin/python")))))
+ '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "/home/hep016/Projects/MARVL/webMARVL/venv/bin/python" :settings "webmarvl.settings.dev") (pony-settings make-pony-project :python "/home/hep016/Projects/connie/connie2testing/venv/bin/python")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
