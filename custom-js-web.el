a573ca75e697c9d2c47b3ecd097f0bbfd731b589
Mark Hepburn <Mark.Hepburn@gmail.com>
2013-05-15 13:18:24 +1000

    Finalise: officially switching to built-in js-mode. Clean up js2 stuff.

--- custom-js-web.el
+++ custom-js-web.el
@@ -2,37 +2,9 @@
 ;; Javascript (and Web) stuff:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; ;;; never mind all that, use Yegge's awesome mode:
-;; (autoload 'js2-mode "js2-mode" nil t)
-;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-;; (setq js2-auto-indent-flag nil)         ; must be set /before/ loading
-;; (setq-default js2-basic-offset 2)       ; (default has changed to
-;;                                         ; c-basic-offset, but I'm used
-;;                                         ; to 2 now)
-;; (setq-default js2-bounce-indent-p t)
-
 ;;; Experiment: now that flycheck+jshint provides error checking, try
 ;;; the built in js-mode for a bit (it's already in auto-mode-alist):
 (setq js-indent-level 2)
-
-;;; highlight-vars-mode from
-;;; http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode/js2-highlight-vars-mode
-(defun mh/js-enable-highlight-vars ()
-  (when (require 'js2-highlight-vars nil t)
-    (js2-highlight-vars-mode)))
-(add-hook 'js2-mode-hook 'mh/js-enable-highlight-vars)
-
-(eval-after-load "js2"
-  '(when (require 'js-comint nil t)
-     (setq inferior-js-program-command "rhino")
-     (add-hook 'js2-mode-hook '(lambda ()
-                                 (local-set-key (kbd "C-c C-z") 'run-js)
-                                 (local-set-key (kbd "C-c C-r") 'js-send-region-and-go)
-                                 (local-set-key (kbd "C-x C-e") 'js-send-last-sexp)
-                                 (local-set-key (kbd "C-M-x")   'js-send-last-sexp-and-go)
-                                 (local-set-key (kbd "C-c b")   'js-send-buffer)
-                                 (local-set-key (kbd "C-c C-b") 'js-send-buffer-and-go)
-                                 (local-set-key (kbd "C-c l")   'js-load-file-and-go)))))
 
 ;;; mozrepl integration
 ;;; (http://people.internetconnection.net/2009/02/interactive-html-development-in-emacs/):
