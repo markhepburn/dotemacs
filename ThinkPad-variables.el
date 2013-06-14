37b906dc73f0d9027b5649abd8027c095607fedb
Mark Hepburn <Mark.Hepburn@gmail.com>
2013-06-14 18:25:25 +1000

    Local vars for el-get

--- ThinkPad-variables.el
+++ ThinkPad-variables.el
@@ -9,7 +9,8 @@
  '(TeX-view-style (quote (("^a5$" "xdvi %d -paper a5") ("^landscape$" "xdvi %d -paper a4r -s 4") ("." "xdvi %d") ("prosper" "open -a /Applications/Preview.app %d"))))
  '(haskell-notify-p t)
  '(haskell-process-type (quote cabal-dev))
- '(haskell-tags-on-save t))
+ '(haskell-tags-on-save t)
+ '(safe-local-variable-values (quote ((require-final-newline . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
