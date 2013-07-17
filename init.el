b1e9e45b1db57e1d69f18057d1ef2dd7120a9ef3
Mark Hepburn <Mark.Hepburn@gmail.com>
2013-07-17 10:26:14 +1000

    Whitespace -- not sure how that got the way it was

--- init.el
+++ init.el
@@ -85,11 +85,11 @@
                :type github
                :pkgname "joddie/pcre2el")
         (:name projectile
-	       :description "Project navigation and management library for Emacs"
-	       :type github
-	       :pkgname "bbatsov/projectile"
-	       :depends (dash grizzl s)
-	       :features projectile)
+               :description "Project navigation and management library for Emacs"
+               :type github
+               :pkgname "bbatsov/projectile"
+               :depends (dash grizzl s)
+               :features projectile)
         (:name sql-indent
                :type emacswiki)
         (:name tags-view
