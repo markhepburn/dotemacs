4c15dede7fe92fda61257c3af17103e453c103fd
Mark Hepburn <Mark.Hepburn@gmail.com>
2013-07-27 23:09:23 +1000

    Make sure erc is loaded before customising it

--- custom-erc.el
+++ custom-erc.el
@@ -1,10 +1,11 @@
-(defun erc-ido-switch-buffer ()
-  "Use ido to switch between active ERC buffers.
+(after 'erc
+  (defun erc-ido-switch-buffer ()
+    "Use ido to switch between active ERC buffers.
 Replaces erc-iswitchb, which isn't working for me at the moment."
-  (interactive)
-  (switch-to-buffer
-   (ido-completing-read "Channel: "
-                        (mapcar 'buffer-name (erc-buffer-list))
-                        nil t)))
+    (interactive)
+    (switch-to-buffer
+     (ido-completing-read "Channel: "
+                          (mapcar 'buffer-name (erc-buffer-list))
+                          nil t)))
 
-(define-key erc-mode-map (kbd "C-c C-b") 'erc-ido-switch-buffer)
+  (define-key erc-mode-map (kbd "C-c C-b") 'erc-ido-switch-buffer))
