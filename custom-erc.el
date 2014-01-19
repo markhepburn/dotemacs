2a23738eab09267bec82fcb129282e9b04f82c4f
Mark Hepburn <Mark.Hepburn@gmail.com>
2014-01-20 07:57:17 +1100

    Adding ERC hide list (JOIN, PART, QUIT)

--- custom-erc.el
+++ custom-erc.el
@@ -1,3 +1,13 @@
+;;; custom-erc.el --- ERC Customisations
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+;;; Ignore all the "xxx has quit: timeout" etc messages:
+(setq erc-hide-list '("JOIN" "PART" "QUIT"))
+
 (after 'erc
   (defun erc-ido-switch-buffer ()
     "Use ido to switch between active ERC buffers.
@@ -9,3 +19,7 @@ Replaces erc-iswitchb, which isn't working for me at the moment."
                           nil t)))
 
   (define-key erc-mode-map (kbd "C-c C-b") 'erc-ido-switch-buffer))
+
+(provide 'custom-erc)
+
+;;; custom-erc.el ends here
