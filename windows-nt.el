12cce9f2006bab9f54c89ef20ae8b79f97330635
Mark Hepburn <Mark.Hepburn@gmail.com>
2014-09-11 10:58:16 +1000

    Function to not display ^M in dos files

--- windows-nt.el
+++ windows-nt.el
@@ -9,6 +9,14 @@
       (re-search-forward (concat varname "=\\([^;]+\\)"))
       (setenv varname (match-string 1)))))
 
+;;; Don't display ^M in mixed-line-endings buffers
+;;; Via http://stackoverflow.com/questions/730751/hiding-m-in-emacs
+(defun remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
+
 ;;; Get shell stuff working properly (such as find-name-dired, etc):
 (setq mh/git-bash-bin "C:/Program Files (x86)/Git/bin")
 
