3bc1ced58a73978c181195dcde7f5ef8fce875cf
Mark Hepburn <Mark.Hepburn@gmail.com>
2014-12-08 12:38:01 +1100

    Rebind mc/edit-lines
    Turns out, you can't use shift as an extra modifer with the control key
    -- it gets ignored (for alphabetical keys).  So use C-! instead.

--- custom-general.el
+++ custom-general.el
@@ -76,7 +76,7 @@
   (diminish 'projectile-mode))
 
 (when (require 'multiple-cursors nil t)
-  (global-set-key (kbd "C-S-C C-S-C") 'mc/edit-lines)
+  (global-set-key (kbd "C-!") 'mc/edit-lines)
   (global-set-key (kbd "C->")     'mc/mark-next-like-this)
   (global-set-key (kbd "C-<")     'mc/mark-previous-like-this)
   (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
