30fdd754efe1a024268f2205e99f4551eed9baa2
Mark Hepburn <Mark.Hepburn@gmail.com>
2016-01-19 10:30:20 +1100

    GNUTLS support

--- windows-nt.el
+++ windows-nt.el
@@ -25,3 +25,7 @@
 
 (setq projectile-indexing-method 'alien
       projectile-enable-caching t)
+
+;;; See http://חנוך.se/diary/how_to_enable_GnuTLS_for_Emacs_24_on_Windows/index.en.html
+;;; Essentially, need to provide the gnutls dlls, and drop the cert bundle in ~/.emacs.d
+(setq-default gnutls-trustfiles (list (concat *mh/lisp-base* "ca-bundle.crt")))
