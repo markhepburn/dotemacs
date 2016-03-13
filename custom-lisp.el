19c7773ab2b88d5440396edee510e0cf48055f5c
Mark Hepburn <Mark.Hepburn@gmail.com>
2016-03-14 09:24:34 +1100

    Clr-refactor: don't warn on project-eval

--- custom-lisp.el
+++ custom-lisp.el
@@ -51,6 +51,7 @@
   ;; :pin melpa-stable
   :after (clojure-mode)
   :init
+  (setq cljr-warn-on-eval nil)
   (add-hook 'clojure-mode-hook (lambda ()
                                  (clj-refactor-mode 1)
                                  (cljr-add-keybindings-with-prefix "C-c C-r"))))
