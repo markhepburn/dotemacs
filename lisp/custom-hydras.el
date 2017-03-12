0457960b156bb2d4a3d6b9e0dda9d0f697551af2
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-03-12 16:37:13 +1100

    Replace win-switch with a defhydra

---
+++ lisp/custom-hydras.el
@@ -0,0 +1,58 @@
+;;; custom-hydras.el --- hydra setup
+
+;;; Commentary:
+;; A few hydras, mostly cribbed from existing examples
+
+;;; Code:
+
+(use-package hydra)
+
+
+;;; win-switch emulation (lighter-weight, and I have hydra installed
+;;; as a dependency anyway):
+
+(defhydra hydra-windows (:color red
+                         :timeout 1.5
+                         :hint nil)
+  "
+  ^Move^   ^Size^          ^Add/Delete^
+--------------------------------------------
+[_h_] ←   [_H_] Wider     [_-_] Split Vert.
+[_j_] ↓   [_J_] Shorter   [_|_] Split Horiz.
+[_k_] ↑   [_K_] Taller    [_0_] Delete
+[_l_] →   [_L_] Narrower  [_1_] Delete Others
+         [_+_] Balance
+"
+  ;; windows movement:
+  ("h" windmove-left)
+  ("j" windmove-down)
+  ("k" windmove-up)
+  ("l" windmove-right)
+  ;; Resizing:
+  ("K" enlarge-window)
+  ("J" shrink-window)
+  ("H" enlarge-window-horizontally)
+  ("L" shrink-window-horizontally)
+  ("+" balance-windows)
+  ;; Adding/Deleting:
+  ("-" split-window-horizontally)
+  ("|" split-window-vertically)
+  ("0" delete-window)
+  ("1" delete-other-windows)
+  ("u" winner-undo)
+  ;; Quit:
+  ("<return>" nil)
+  ("q" nil))
+
+(defun mh/maybe-hydra-windows (arg)
+  (interactive "P")
+  (if (or arg (> (count-windows) 2))
+      (call-interactively 'hydra-windows/body)
+    (call-interactively 'other-window)))
+
+(global-set-key (kbd "C-x o") 'mh/maybe-hydra-windows)
+
+
+(provide 'custom-hydras)
+
+;;; custom-hydras.el ends here
