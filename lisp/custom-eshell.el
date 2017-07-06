75ca60359bbe668d71680da24b12f159d63898a9
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-07-06 13:41:07 +1000

    eshell: open files in "ls" output by return/clicking

--- lisp/custom-eshell.el
+++ lisp/custom-eshell.el
@@ -78,6 +78,38 @@
     (erase-buffer)
     (eshell-send-input)))
 
+;;; type "ls" then open files by clicking / hitting <return>
+;;; http://caiorss.github.io/Emacs-Elisp-Programming/Eshell.html
+(eval-after-load "em-ls"
+  '(progn
+     (defun ted-eshell-ls-find-file-at-point (point)
+       "RET on Eshell's `ls' output to open files."
+       (interactive "d")
+       (find-file (buffer-substring-no-properties
+                   (previous-single-property-change point 'help-echo)
+                   (next-single-property-change point 'help-echo))))
+
+     (defun pat-eshell-ls-find-file-at-mouse-click (event)
+       "Middle click on Eshell's `ls' output to open files.
+ From Patrick Anderson via the wiki."
+       (interactive "e")
+       (ted-eshell-ls-find-file-at-point (posn-point (event-end event))))
+
+     (let ((map (make-sparse-keymap)))
+       (define-key map (kbd "RET")      'ted-eshell-ls-find-file-at-point)
+       (define-key map (kbd "<return>") 'ted-eshell-ls-find-file-at-point)
+       (define-key map (kbd "<mouse-2>") 'pat-eshell-ls-find-file-at-mouse-click)
+       (defvar ted-eshell-ls-keymap map))
+
+     (defadvice eshell-ls-decorated-name (after ted-electrify-ls activate)
+       "Eshell's `ls' now lets you click or RET on file names to open them."
+       (add-text-properties 0 (length ad-return-value)
+                            (list 'help-echo "RET, mouse-2: visit this file"
+                                  'mouse-face 'highlight
+                                  'keymap ted-eshell-ls-keymap)
+                            ad-return-value)
+       ad-return-value)))
+
 ;;; smart display (allow easier editing of command lines
 ;;; https://www.masteringemacs.org/article/complete-guide-mastering-eshell
 ;;; (Removed for now; didn't like it that much)
