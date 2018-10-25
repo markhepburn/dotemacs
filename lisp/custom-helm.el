084a1dde77ab03e6fdd735db226ca10ef3bc3ae5
Mark Hepburn <Mark.Hepburn@gmail.com>
2018-10-26 10:30:09 +1100

    Need the line-number option for helm-ag to work properly

--- lisp/custom-helm.el
+++ lisp/custom-helm.el
@@ -92,7 +92,7 @@
 
 ;;; Helm-ag:
 (use-package helm-ag
-  :init (setq helm-ag-base-command "rg --no-heading --smart-case"; "ag --nocolor --nogroup --ignore-case"
+  :init (setq helm-ag-base-command "rg --no-heading --smart-case --line-number"; "ag --nocolor --nogroup --ignore-case"
               ;; helm-ag-command-option "--all-text"
               helm-ag-thing-at-point 'symbol)
   :bind (("M-g ." . helm-ag)
