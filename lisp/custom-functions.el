97154ed7a32043f26ebe0f66166c79d95980fcbe
Mark Hepburn <Mark.Hepburn@gmail.com>
2020-03-09 12:24:51 +1100

    Improved keyboard-quit function thanks to Reddit

--- lisp/custom-functions.el
+++ lisp/custom-functions.el
@@ -331,6 +331,38 @@ the right thing if the file is under version control."
             (kill-buffer buffer)
             (message "File '%s' successfully removed" filename)))))))
 
+;;; https://with-emacs.com/posts/tips/quit-current-context/
+(defun keyboard-quit-context+ ()
+  "Quit current context.
+
+    This function is a combination of `keyboard-quit' and
+    `keyboard-escape-quit' with some parts omitted and some custom
+    behavior added."
+  (interactive)
+  (cond ((region-active-p)
+         ;; Avoid adding the region to the window selection.
+         (setq saved-region-selection nil)
+         (let (select-active-regions)
+           (deactivate-mark)))
+        ((eq last-command 'mode-exited) nil)
+        (current-prefix-arg
+         nil)
+        (defining-kbd-macro
+          (message
+           (substitute-command-keys
+            "Quit is ignored during macro defintion, use \\[kmacro-end-macro] if you want to stop macro definition"))
+          (cancel-kbd-macro-events))
+        ((active-minibuffer-window)
+         (when (get-buffer-window "*Completions*")
+           ;; hide completions first so point stays in active window when
+           ;; outside the minibuffer
+           (minibuffer-hide-completions))
+         (abort-recursive-edit))
+        (t
+         (keyboard-quit))))
+
+(global-set-key [remap keyboard-quit] #'keyboard-quit-context+)
+
 (defun mh/tag-posts (tags)
   "Apply tags (specified interactively) to the files marked in
 dired, merging with existing tags.  Assumes the use of
