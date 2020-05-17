b64ebd61a44741fe8d86e24cf4af6e59e0b38d38
Mark Hepburn <Mark.Hepburn@gmail.com>
2020-05-18 09:11:08 +1000

    Add Nix interaction packages

---
+++ lisp/custom-nix.el
@@ -0,0 +1,14 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; custom-c.el --- Customisations for Nix package manager
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Code:
+
+(use-package nix-mode
+  :mode "\\.nix\\'")
+
+(use-package nix-buffer)
+
+(provide 'custom-nix)
+
+;;; custom-nix.el ends here
