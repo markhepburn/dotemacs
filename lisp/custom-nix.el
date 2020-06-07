8e4dfce4e29f2e1908dc6aaa1a67141a53a1778c
Mark Hepburn <Mark.Hepburn@gmail.com>
2020-06-07 18:20:27 +1000

    Expanded nix-interop configuration

--- lisp/custom-nix.el
+++ lisp/custom-nix.el
@@ -6,7 +6,18 @@
 
 (use-package nix-mode
   :mode "\\.nix\\'")
+(use-package nix-drv-mode
+  :ensure nix-mode
+  :mode "\\.drv\\'")
+(use-package nix-shell
+  :ensure nix-mode
+  :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
+(use-package nix-repl
+  :ensure nix-mode
+  :commands (nix-repl))
 
+;;; Loads dir-locals.nix (by default)
+;;; See https://blog.jethro.dev/posts/nix_buffer_emacs/ for sample usage:
 (use-package nix-buffer)
 
 (provide 'custom-nix)
