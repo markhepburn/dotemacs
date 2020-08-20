c111a186709f903dd869165a331a2475409604ff
Mark Hepburn <Mark.Hepburn@gmail.com>
2020-08-20 11:44:32 +1000

    Switch from escreen to tabs
    Everything I want in tabs, including the all-import per-tab
    recent-buffers stack, plus escreen is abandoned.  (Still amazingly
    effective for its size and age though!)
    
    I may still want to customise the prefix though; "C-x t" is ok but a
    bit fiddly for frequent usage.  Probably reclaim C-z again.

---
+++ lisp/custom-tabs.el
@@ -0,0 +1,17 @@
+;;; custom-tabs.el --- Customisations for tab-bar-mode
+
+;;; Commentary:
+;;;
+
+;;; Code:
+
+(use-package tab-bar
+  :init
+  (setq tab-bar-show 1                  ; hide when only one tab
+        tab-bar-new-tab-choice "*scratch*"
+        tab-bar-tab-name-function #'tab-bar-tab-name-all
+        tab-bar-new-tab-to 'rightmost)
+  :config
+  ;; use C-x t T to toggle actually displaying the tab-bar:
+  (define-key tab-prefix-map (kbd "T") #'tab-bar-mode))
+
