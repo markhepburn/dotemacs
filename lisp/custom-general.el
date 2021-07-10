fc56b6d12366eab2550012e288ad7ffa6fd18aea
Mark Hepburn <Mark.Hepburn@gmail.com>
2021-07-10 11:09:12 +1000

    Add inferior-emacs-lisp (ielm) to modes that get eldoc enabled

--- lisp/custom-general.el
+++ lisp/custom-general.el
@@ -460,7 +460,7 @@
             (auto-fill-mode -1)))
 
 ;; Show docs where available:
-(enable-minor-mode-for eldoc-mode '(emacs-lisp lisp inferior-lisp))
+(enable-minor-mode-for eldoc-mode '(emacs-lisp lisp inferior-lisp inferior-emacs-lisp))
 (after 'eldoc (diminish 'eldoc-mode))
 
 ;; view pdfs etc inline:
