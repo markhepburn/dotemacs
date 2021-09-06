4d09020bf23a0f0d27a747e7f48c0a5cbcb378b5
Mark Hepburn <Mark.Hepburn@gmail.com>
2021-09-06 11:30:06 +1000

    Add direnv integration

--- lisp/custom-general.el
+++ lisp/custom-general.el
@@ -32,6 +32,9 @@
   :diminish editorconfig-mode
   :config (editorconfig-mode 1))
 
+(use-package direnv
+  :config (direnv-mode))
+
 ;;; Create dir-locals file with
 ;;; ((yaml-mode
 ;;;   (ansible-vault-password-file . "/home/notroot/.ansible-vault/custom_vault_pass")))
