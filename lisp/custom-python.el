f0766ab3f211fed331ff85f60d843229bec79dd3
Mark Hepburn <Mark.Hepburn@gmail.com>
2021-09-16 17:41:45 +1000

    Add lsp support for python via jedi-language-server

--- lisp/custom-python.el
+++ lisp/custom-python.el
@@ -21,9 +21,17 @@
         elpy-django-server-command "runserver_plus")
   ;; May need to setenv WORKON_HOME:
   (defalias 'workon 'pyvenv-workon)
+  :hook (python-mode . lsp)
   :config
   (delete 'elpy-module-highlight-indentation elpy-modules)
   (delete 'elpy-module-flymake elpy-modules))
+
+;;; pipx install jedi-language-server:
+(use-package lsp-jedi
+  :after lsp-mode
+  :demand t ; Need this, because nothing is autoloaded so otherwise it doesn't get loaded
+  :config
+  (add-to-list 'lsp-disabled-clients 'pyls))
 
 ;;; ipython-notebook integration:
 (use-package ein
