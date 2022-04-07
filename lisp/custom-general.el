c2bbb292fca28d371983cc3972acb515a31a7b35
Mark Hepburn <Mark.Hepburn@gmail.com>
2022-04-07 12:34:26 +1000

    Tidy: use-package for ibuffer

--- lisp/custom-general.el
+++ lisp/custom-general.el
@@ -517,17 +517,16 @@
          ("C-h v" . helpful-variable)))
 
 ;; Used to use bs-show, but the ibuffer emulation does it all and more:
-(when (require 'ibuf-ext nil t)
-  (global-set-key (kbd "C-x C-b") 'ibuffer-bs-show))
-(setq-default ibuffer-default-sorting-mode 'major-mode)
-(add-hook 'ibuffer-mode-hook (lambda () (hl-line-mode 1)))
+(use-package ibuf-ext
+  :ensure nil
+  :config (setq-default ibuffer-default-sorting-mode 'major-mode)
+  :bind (("C-x C-b" . ibuffer-bs-show)))
 (use-package ibuffer-vc
-  :config
-  (add-hook 'ibuffer-hook
-            (lambda ()
-              (ibuffer-vc-set-filter-groups-by-vc-root)
-              (unless (eq ibuffer-sorting-mode 'alphabetic)
-                (ibuffer-do-sort-by-alphabetic)))))
+  :hook
+  (ibuffer . (lambda ()
+               (ibuffer-vc-set-filter-groups-by-vc-root)
+               (unless (eq ibuffer-sorting-mode 'alphabetic)
+                 (ibuffer-do-sort-by-alphabetic)))))
 
 ;; let's play with using C-w to delete words backwards:
 (global-set-key (kbd "C-w")     'backward-kill-word)
