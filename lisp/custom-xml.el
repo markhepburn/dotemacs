caaaa1ab33758b2cdf5a446fc9e270d2f4ca64b8
Mark Hepburn <Mark.Hepburn@gmail.com>
2022-04-07 17:11:13 +1000

    use-package: xml tidy
    A shame that we can't evaluate code in the :mode clause

--- lisp/custom-xml.el
+++ lisp/custom-xml.el
@@ -6,37 +6,33 @@
 ;;; nxml-mode should be auto-loaded automatically I think:
 
 ;;; Commentary:
-;; 
+;;
 
 ;;; Code:
 
-(add-to-list 'auto-mode-alist
-             (cons (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss" "kml" "kmz") t) "\\'")
-                   'nxml-mode))
-(fset 'xml-mode 'nxml-mode)
-;;; Hook(s):
-(defun mh/nxml-enable-hs ()
-  "Functions to run when in nxml mode."
-  (setq nxml-sexp-element-flag t)
-  (after "hideshow"
-    (let ((nxml-mode-hs-info '(nxml-mode ("^\\s-*\\(<[^/].*>\\)\\s-*$" 1)
-                                         "^\\s-*</.*>\\s-*$")))
-      (when (not (member nxml-mode-hs-info hs-special-modes-alist))
-        (setq hs-special-modes-alist
-              (cons nxml-mode-hs-info hs-special-modes-alist))
-        t)))
-  (hs-minor-mode 1))
-
-(add-hook 'nxml-mode-hook 'mh/nxml-enable-hs)
-;; (add-hook 'nxml-mode-hook
-;;           ;; alternatively, see variable nxml-sexp-element-flag
-;;           ;; Actually, C-M-n/p does what I wanted this for I think!
-;;           (lambda () (local-set-key (kbd "C-c M-f") 'nxml-forward-balanced-item)))
-;;; General setup:
-(setq nxhtml-global-minor-mode t)
-(setq nxhtml-global-validation-header-mode t)
-(setq nxhtml-skip-welcome t)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package nxml-mode
+  :ensure nil
+  ;; (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss" "kml" "kmz") t) "\\'")
+  :mode "\\.\\(km[lz]\\|r\\(?:ng\\|ss\\)\\|s\\(?:ch\\|vg\\)\\|x\\(?:ml\\|s\\(?:d\\|lt\\)\\)\\)\\'"
+  :init
+  (setq nxhtml-global-minor-mode t
+        nxhtml-global-validation-header-mode t
+        nxhtml-skip-welcome t)
+  :config
+  (fset 'xml-mode 'nxml-mode)
+  ;; Hook(s):
+  (defun mh/nxml-enable-hs ()
+    "Functions to run when in nxml mode."
+    (setq nxml-sexp-element-flag t)
+    (after "hideshow"
+      (let ((nxml-mode-hs-info '(nxml-mode ("^\\s-*\\(<[^/].*>\\)\\s-*$" 1)
+                                           "^\\s-*</.*>\\s-*$")))
+        (when (not (member nxml-mode-hs-info hs-special-modes-alist))
+          (setq hs-special-modes-alist
+                (cons nxml-mode-hs-info hs-special-modes-alist))
+          t)))
+    (hs-minor-mode 1))
+  :hook (nxml-mode . mh/nxml-enable-hs))
 
 (provide 'custom-xml)
 
