4dedd3b3ef18fe17d01ebeb1137aea4ddea5a69b
Mark Hepburn <Mark.Hepburn@gmail.com>
2022-04-07 16:21:25 +1000

    use-package tidy: use :bind

--- lisp/custom-tabs.el
+++ lisp/custom-tabs.el
@@ -11,15 +11,15 @@
         tab-bar-new-tab-choice "*scratch*"
         tab-bar-tab-name-function #'tab-bar-tab-name-all
         tab-bar-new-tab-to 'rightmost)
-  :config
-  ;; use C-x t T to toggle actually displaying the tab-bar:
-  (define-key tab-prefix-map (kbd "T") #'toggle-frame-tab-bar)
-  ;; C-z C-z to emulate "toggle between most recent tab" (not circulate in order)
-  (define-key tab-prefix-map (kbd "C-z") #'tab-recent)
-  ;; View (/select) list of tabs. See also C-zRET tab-bar-select-tab-by-name
-  (define-key tab-prefix-map (kbd "C-l") #'tab-list)
-  ;; Next/previous tabs:
-  (define-key tab-prefix-map (kbd "C-n") #'tab-next)
-  (define-key tab-prefix-map (kbd "C-p") #'tab-previous)
+  :bind (:map tab-prefix-map
+              ;; use C-x t T to toggle actually displaying the tab-bar:
+              ("T" . toggle-frame-tab-bar)
+              ;; C-z C-z to emulate "toggle between most recent tab" (not circulate in order)
+              ("C-z" . tab-recent)
+              ;; View (/select) list of tabs. See also C-zRET tab-bar-select-tab-by-name
+              ("C-l" . tab-list)
+              ;; Next/previous tabs:
+              ("C-n" . tab-next)
+              ("C-p" . tab-previous))
   :bind-keymap ("C-z" . tab-prefix-map))
 
