90012d78c7f40af36b4f90e55d9b9d268dcc9431
Mark Hepburn <Mark.Hepburn@gmail.com>
2022-12-09 19:10:14 +1100

    Combine tree-sitter folding and hs-show under common bindings

--- lisp/custom-general.el
+++ lisp/custom-general.el
@@ -197,34 +197,42 @@
     (interactive "p")
     (let (message-log-max
           (inhibit-message t))
-      (if (= level 1)
-          (pcase last-command
-            ('hs-cycle
-             (hs-hide-level 1)
-             (setq this-command 'hs-cycle-children))
-            ('hs-cycle-children
-             ;; TODO: Fix this case. `hs-show-block' needs to be
-             ;; called twice to open all folds of the parent
-             ;; block.
-             (save-excursion (hs-show-block))
-             (hs-show-block)
-             (setq this-command 'hs-cycle-subtree))
-            ('hs-cycle-subtree
-             (hs-hide-block))
-            (_
-             (if (not (hs-already-hidden-p))
-                 (hs-hide-block)
+      (if (and (fboundp 'tree-sitter-mode) tree-sitter-mode)
+          (ts-fold-toggle)
+        (if (= level 1)
+            (pcase last-command
+              ('hs-cycle
                (hs-hide-level 1)
-               (setq this-command 'hs-cycle-children))))
-        (hs-hide-level level)
-        (setq this-command 'hs-hide-level))))
+               (setq this-command 'hs-cycle-children))
+              ('hs-cycle-children
+               ;; TODO: Fix this case. `hs-show-block' needs to be
+               ;; called twice to open all folds of the parent
+               ;; block.
+               (save-excursion (hs-show-block))
+               (hs-show-block)
+               (setq this-command 'hs-cycle-subtree))
+              ('hs-cycle-subtree
+               (hs-hide-block))
+              (_
+               (if (not (hs-already-hidden-p))
+                   (hs-hide-block)
+                 (hs-hide-level 1)
+                 (setq this-command 'hs-cycle-children))))
+          (hs-hide-level level)
+          (setq this-command 'hs-hide-level)))))
   (defun hs-global-cycle ()
     (interactive)
-    (pcase last-command
-      ('hs-global-cycle
-       (save-excursion (hs-show-all))
-       (setq this-command 'hs-global-show))
-      (_ (hs-hide-all))))
+    (if (and (fboundp 'tree-sitter-mode) tree-sitter-mode)
+        (pcase last-command
+          ('hs-global-cycle
+           (save-excursion (ts-fold-open-all))
+           (setq this-command 'ts-fold-open-all))
+          (_ (ts-fold-close-all)))
+      (pcase last-command
+        ('hs-global-cycle
+         (save-excursion (hs-show-all))
+         (setq this-command 'hs-global-show))
+        (_ (hs-hide-all)))))
   (enable-minor-mode-for hs-minor-mode '(prog))
   :diminish hs-minor-mode
   :bind (("C-<tab>" . hs-cycle)
