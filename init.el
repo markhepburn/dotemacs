572c7fffdbd0f4185e67925d4d59cba8f570f550
Mark Hepburn <Mark.Hepburn@gmail.com>
2022-12-18 14:13:28 +1100

    Add cribbed utility to wrap package-vc-install

--- init.el
+++ init.el
@@ -75,6 +75,27 @@ file (including following symlinks).")
 (setq use-package-always-ensure t
       use-package-verbose t)
 
+;;; https://tony-zorman.com/posts/2022-11-30-package-vc-install.html
+(cl-defun pkg-help/vc-install (&key (fetcher "github") repo name rev backend)
+  "Install a package from a remote if it's not already installed.
+This is a thin wrapper around `package-vc-install' in order to
+make non-interactive usage more ergonomic.  Takes the following
+named arguments:
+
+- FETCHER the remote where to get the package (e.g., \"gitlab\").
+  If omitted, this defaults to \"github\".
+
+- REPO should be the name of the repository (e.g.,
+  \"slotThe/arXiv-citation\".
+
+- NAME, REV, and BACKEND are as in `package-vc-install' (which
+  see)."
+  (let* ((url (format "https://www.%s.com/%s" fetcher repo))
+         (iname (when name (intern name)))
+         (pac-name (or iname (intern (file-name-base repo)))))
+    (unless (package-installed-p pac-name)
+      (package-vc-install url iname rev backend))))
+
 ;;; For toggle-case, optionally others not on elpa:
 (use-package quelpa-use-package
   :init (setq quelpa-update-melpa-p nil)
