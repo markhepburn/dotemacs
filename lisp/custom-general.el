b47f0be42eb71efe48d8bf4553424ef42cb46300
Mark Hepburn <Mark.Hepburn@gmail.com>
2023-04-01 11:31:34 +1100

    Dired tweaks (cribbed from a blog post)

--- lisp/custom-general.el
+++ lisp/custom-general.el
@@ -431,6 +431,7 @@
   :init
   (setq dired-recursive-deletes 'top
         dired-recursive-copies 'top
+        dired-listing-switches "-alh --time-style=long-iso"
         dired-dwim-target t)
   :config
   ;; (http://whattheemacsd.com/setup-dired.el-02.html)
@@ -442,6 +443,41 @@
     (interactive)
     (goto-char (point-max))
     (dired-next-line -1))
+  ;; https://www.n16f.net/blog/decluttering-dired-for-peace-of-mind/
+  (defun mh/dired-postprocess-ls-output ()
+    "Postprocess the list of files printed by the ls program when
+executed by Dired."
+    (save-excursion
+      (goto-char (point-min))
+      (while (not (eobp))
+        ;; Go to the beginning of the next line representing a file
+        (while (null (dired-get-filename nil t))
+          (dired-next-line 1))
+        (beginning-of-line)
+        ;; Narrow to the line and process it
+        (let ((start (line-beginning-position))
+              (end (line-end-position)))
+          (save-restriction
+            (narrow-to-region start end)
+            (setq inhibit-read-only t)
+            (unwind-protect
+                (mh/dired-postprocess-ls-line)
+              (setq inhibit-read-only nil))))
+        ;; Next line
+        (dired-next-line 1))))
+  (defun mh/dired-postprocess-ls-line ()
+    "Postprocess a single line in the ls output, i.e. the information
+about a single file. This function is called with the buffer
+narrowed to the line."
+    ;; Highlight everything but the filename
+    (when (re-search-forward directory-listing-before-filename-regexp nil t 1)
+      (add-text-properties (point-min) (match-end 0) '(font-lock-face shadow)))
+    ;; Hide the link count
+    ;; (beginning-of-line)
+    ;; (when (re-search-forward " +[0-9]+" nil t 1)
+    ;;   (add-text-properties (match-beginning 0) (match-end 0) '(invisible t)))
+    )
+  :hook (dired-after-readin . mh/dired-postprocess-ls-output)
   :bind (:map dired-mode-map
               ([remap beginning-of-buffer] . dired-back-to-top)
               ([remap end-of-buffer] . dired-jump-to-bottom)))
