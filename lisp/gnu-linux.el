dfc0dffdf5e279f7a6a484fcdcf022ab06a7eeca
Mark Hepburn <Mark.Hepburn@gmail.com>
2024-01-06 09:32:57 +1100

    Remove local path to elixir-ls; just use the lsp-installed version

--- lisp/gnu-linux.el
+++ lisp/gnu-linux.el
@@ -115,8 +115,6 @@
 
 (add-to-list 'exec-path (expand-file-name "~/.local/bin"))
 
-(add-to-list 'exec-path (expand-file-name "~/Projects/elixir-ls-git/release-1.16"))
-
 ;;; Flutter support for dart-lsp
 (setq lsp-dart-flutter-sdk-dir (expand-file-name "~/Vendor/flutter/"))
 (setq flutter-sdk-path lsp-dart-flutter-sdk-dir)
