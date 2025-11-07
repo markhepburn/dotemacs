9d1de6ec705720612fe2d8f60f416a124f41609e
Mark Hepburn <Mark.Hepburn@gmail.com>
2025-11-07 22:14:33 +1100

    Remove package `try'; conflicts with quickstart
    I've had issues where I've use 'try', and the next time restarting
    Emacs it hasn't loaded my init, because package-quickstart.el now only
    contains the package "tried".  I've had a quick look and I'm not sure
    it's solveable: `package-install' does a refresh, which either saves
    the (now singleton) package-archives list, or it nukes the file.
    
    I may submit a bug if I can confirm it.

--- lisp/custom-general.el
+++ lisp/custom-general.el
@@ -12,8 +12,6 @@
 (require 'saveplace)
 ;;; Code:
 
-(use-package try :commands (try))
-
 ;;; Some miscellaneous packages, no bindings or anything yet:
 (use-package diminish :commands (diminish))
 (use-package free-keys :commands (free-keys))
