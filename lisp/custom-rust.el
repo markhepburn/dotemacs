6b84c24898ec07daf85529d5f85738676152f1a2
Mark Hepburn <Mark.Hepburn@gmail.com>
2025-12-06 18:50:08 +1100

    tidy: rust config still used lsp-mode via rustic
    Ie, rustic assumes you're using lsp, but defaults to lsp-mode.  Change
    that to eglot and all is good, plus I can remove some config

--- lisp/custom-rust.el
+++ lisp/custom-rust.el
@@ -8,27 +8,9 @@
 ;;; Based off https://robert.kra.hn/posts/2021-02-07_rust-with-emacs/
 
 (use-package rustic
-  ;; :bind (:map rustic-mode-map
-  ;;             ("M-j" . lsp-ui-imenu)
-  ;;             ("M-?" . lsp-find-references)
-  ;;             ("C-c C-c l" . flycheck-list-errors)
-  ;;             ("C-c C-c a" . lsp-execute-code-action)
-  ;;             ("C-c C-c r" . lsp-rename)
-  ;;             ("C-c C-c q" . lsp-workspace-restart)
-  ;;             ("C-c C-c Q" . lsp-workspace-shutdown)
-  ;;             ("C-c C-c s" . lsp-rust-analyzer-status))
-  :custom
-  ;; what to use when checking on-save. "check" is default, I prefer clippy
-  (lsp-rust-analyzer-cargo-watch-command "clippy")
-  (lsp-eldoc-render-all t)
-  (lsp-idle-delay 0.6)
-  (lsp-inlay-hint-enable t)
+  :init
+  (setq rustic-lsp-client 'eglot)
   :config
-  ;; uncomment for less flashiness
-  ;; (setq lsp-eldoc-hook nil)
-  ;; (setq lsp-enable-symbol-highlighting nil)
-  ;; (setq lsp-signature-auto-activate nil)
-
   ;; comment to disable rustfmt on save
   (setq rustic-format-on-save t)
   :mode ("\\.rust\\'" . rustic-mode)
@@ -40,8 +22,7 @@
   ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
   ;; no longer be necessary.
   (when buffer-file-name
-    (setq-local buffer-save-without-query t))
-  (lsp-inlay-hints-mode 1))
+    (setq-local buffer-save-without-query t)))
 
 (provide 'custom-rust)
 
